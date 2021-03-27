e2bda80dfb3a6f840c5208b18f4e07eb43f0b8a4
Grandon Smith <grandon.smith@yahoo.com>
2021-03-26 21:39:22 -0400

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import compile from '../compile'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+ 
+export default App;
