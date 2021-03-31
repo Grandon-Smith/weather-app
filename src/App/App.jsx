57845f67ece73c4ab857763a9deea721cb40269f
Grandon Smith <grandon.smith@yahoo.com>
2021-03-30 21:14:28 -0400

    adjusting front end to accomodate new weather shape

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import LocationForm from '../LocationForm.jsx'
-import {
-  // Switch,
-  // Route,
-  // Link,
-  withRouter
-} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -22,10 +17,8 @@ class App extends Component {
   }
 
   setBackground(data) {
-    // let bkg = 'default';
       let bkg;
       const { id } = data.weather[0]
-      // console.log(id, data.weather[0].id)
       if(id < 300) {
         bkg = 'stormy';
       } else if(id < 600) {
@@ -47,9 +40,8 @@ class App extends Component {
       alert('you need to choose a city and state')
       return
     }
-    // this.props.history.push(`/weather?city=${city}&state=${state}`)
     let url = 'http://localhost:8000/weather'
-    const weatherData = await fetch(url, {
+    const allWeatherData = await fetch(url, {
       method: 'POST',
       mode: 'cors',
       // credentials: '*',
@@ -73,12 +65,12 @@ class App extends Component {
       return data
     })
 
-    if(weatherData.error) {
-      this.setState({error: weatherData.error})
+    if(allWeatherData.error) {
+      this.setState({error: allWeatherData.error})
     } else {
-      let bkg = this.setBackground(weatherData)
+      let bkg = this.setBackground(allWeatherData.firstWeatherData)
       this.setState({
-        weatherData: weatherData,
+        weatherData: allWeatherData,
         background: bkg,
       })
     }
@@ -93,9 +85,17 @@ class App extends Component {
   }
 
   render() {
-    const { weatherData, background, error } = this.state
+    console.log('App render', this.state)
+    const { weatherData, background, error } = this.state;
+    let firstWeatherData;
+    let moreWeatherData;
+    if(weatherData !== null) {
+      firstWeatherData = this.state.weatherData.firstWeatherData;
+      moreWeatherData = this.state.weatherData.moreWeatherData;
+      console.log(moreWeatherData)
+    }
+
     const placeHolder = "--"
-    // console.log('App render')
 
     return (
       <div className={`App ${background} `}>
@@ -110,33 +110,57 @@ class App extends Component {
           <section className="sec-1">
             <div className="error">{error ? error : ""}</div>
             <div className="location">
-              <h2>{weatherData == null ? "City" : weatherData.name}</h2>
+              <h2>{weatherData == null ? "City" : firstWeatherData.name}</h2>
             </div>
             <div className="sub-temps">
-              <p>High: {weatherData == null ? placeHolder : Math.round(weatherData.main.temp_max)}°</p>
-              <p>Low: {weatherData == null ? placeHolder : Math.round(weatherData.main.temp_min)}°</p>
+              <p>High: {weatherData == null ? placeHolder : Math.round(firstWeatherData.main.temp_max)}°</p>
+              <p>Low: {weatherData == null ? placeHolder : Math.round(firstWeatherData.main.temp_min)}°</p>
             </div>
             <div className="curr-temp">
               <p>Current Temp</p>
-              <h2>{weatherData == null ? placeHolder : Math.round(weatherData.main.temp)}°</h2>
-              <p>{weatherData == null ? '' : weatherData.weather[0].description}</p>
+              <h2>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.temp)}°</h2>
+              <p>{weatherData == null ? '' : firstWeatherData.weather[0].description}</p>
             </div>
           </section>
   
           <section className="sec-2">
             <div className="sec-2-1">
               <p>Humidity:</p>
-              <p>{weatherData == null ? placeHolder : Math.round(weatherData.main.humidity)}%</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.humidity)}%</p>
             </div>
             <div className="sec-2-2">
               <p>Wind:</p>
-              <p>{weatherData == null ? placeHolder : Math.round(weatherData.wind.speed)}mph</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.wind.speed)}mph</p>
             </div>
             <div className="sec-2-3">
               <p>Feels Like:</p>
-              <p>{weatherData == null ? placeHolder : Math.round(weatherData.main.feels_like)}°</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.feels_like)}°</p>
             </div>
           </section>
+
+          <section className="sec-2">
+            <div className="sec-3-1">
+              <p>Humidity:</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.humidity)}%</p>
+            </div>
+            <div className="sec-3-2">
+              <p>Wind:</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.wind.speed)}mph</p>
+            </div>
+            <div className="sec-3-3">
+              <p>Feels Like:</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.feels_like)}°</p>
+            </div>
+            <div className="sec-3-3">
+              <p>Feels Like:</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.feels_like)}°</p>
+            </div>
+            <div className="sec-3-3">
+              <p>Feels Like:</p>
+              <p>{weatherData == null ? placeHolder : Math.round(firstWeatherData.main.feels_like)}°</p>
+            </div>
+          </section>
+          
         </main>
         <footer>
           Powered By <a href="https://openweathermap.org/api">Open Weather Map</a>
